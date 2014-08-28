17300a7d1c0875b2aa3ae72b5bf4261d770e0dfe
LukeMike <luca.micheletti.laser@gmail.com>
2014-08-28 17:55:29 +0200

    corrected settings of some timer and added filter for PPMSUM

--- src/drivers/stm32/drv_hrt.c
+++ src/drivers/stm32/drv_hrt.c
@@ -94,7 +94,7 @@
 #elif HRT_TIMER == 3
 # define HRT_TIMER_BASE		STM32_TIM3_BASE
 # define HRT_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define HRT_TIMER_POWER_BIT	RCC_APB2ENR_TIM3EN
+# define HRT_TIMER_POWER_BIT	RCC_APB1ENR_TIM3EN
 # define HRT_TIMER_VECTOR	STM32_IRQ_TIM3
 # define HRT_TIMER_CLOCK	STM32_APB1_TIM3_CLKIN
 # if CONFIG_STM32_TIM3
@@ -103,7 +103,7 @@
 #elif HRT_TIMER == 4
 # define HRT_TIMER_BASE		STM32_TIM4_BASE
 # define HRT_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define HRT_TIMER_POWER_BIT	RCC_APB2ENR_TIM4EN
+# define HRT_TIMER_POWER_BIT	RCC_APB1ENR_TIM4EN
 # define HRT_TIMER_VECTOR	STM32_IRQ_TIM4
 # define HRT_TIMER_CLOCK	STM32_APB1_TIM4_CLKIN
 # if CONFIG_STM32_TIM4
@@ -112,7 +112,7 @@
 #elif HRT_TIMER == 5
 # define HRT_TIMER_BASE		STM32_TIM5_BASE
 # define HRT_TIMER_POWER_REG	STM32_RCC_APB1ENR
-# define HRT_TIMER_POWER_BIT	RCC_APB2ENR_TIM5EN
+# define HRT_TIMER_POWER_BIT	RCC_APB1ENR_TIM5EN
 # define HRT_TIMER_VECTOR	STM32_IRQ_TIM5
 # define HRT_TIMER_CLOCK	STM32_APB1_TIM5_CLKIN
 # if CONFIG_STM32_TIM5
@@ -294,40 +294,40 @@ static void		hrt_call_invoke(void);
 # endif
 
 # if HRT_PPM_CHANNEL == 1
-#  define rCCR_PPM	rCCR1			/* capture register for PPM */
-#  define DIER_PPM	GTIM_DIER_CC1IE		/* capture interrupt (non-DMA mode) */
+#  define rCCR_PPM		rCCR1				/* capture register for PPM */
+#  define DIER_PPM		GTIM_DIER_CC1IE		/* capture interrupt (non-DMA mode) */
 #  define SR_INT_PPM	GTIM_SR_CC1IF		/* capture interrupt (non-DMA mode) */
 #  define SR_OVF_PPM	GTIM_SR_CC1OF		/* capture overflow (non-DMA mode) */
-#  define CCMR1_PPM	1			/* not on TI1/TI2 */
-#  define CCMR2_PPM	0			/* on TI3, not on TI4 */
-#  define CCER_PPM	(GTIM_CCER_CC1E | GTIM_CCER_CC1P | GTIM_CCER_CC1NP) /* CC1, both edges */
+#  define CCMR1_PPM		(1 | 0x03 << 4)		/* not on TI1/TI2 */
+#  define CCMR2_PPM		0					/* on TI3, not on TI4 */
+#  define CCER_PPM		(GTIM_CCER_CC1E | GTIM_CCER_CC1P | GTIM_CCER_CC1NP) /* CC1, both edges */
 #  define CCER_PPM_FLIP	GTIM_CCER_CC1P
 # elif HRT_PPM_CHANNEL == 2
-#  define rCCR_PPM	rCCR2			/* capture register for PPM */
-#  define DIER_PPM	GTIM_DIER_CC2IE		/* capture interrupt (non-DMA mode) */
+#  define rCCR_PPM		rCCR2				/* capture register for PPM */
+#  define DIER_PPM		GTIM_DIER_CC2IE		/* capture interrupt (non-DMA mode) */
 #  define SR_INT_PPM	GTIM_SR_CC2IF		/* capture interrupt (non-DMA mode) */
 #  define SR_OVF_PPM	GTIM_SR_CC2OF		/* capture overflow (non-DMA mode) */
-#  define CCMR1_PPM	2			/* not on TI1/TI2 */
-#  define CCMR2_PPM	0			/* on TI3, not on TI4 */
-#  define CCER_PPM	(GTIM_CCER_CC2E | GTIM_CCER_CC2P | GTIM_CCER_CC2NP) /* CC2, both edges */
+#  define CCMR1_PPM		(2 | 0x03 << 4)		/* not on TI1/TI2 */
+#  define CCMR2_PPM		0					/* on TI3, not on TI4 */
+#  define CCER_PPM		(GTIM_CCER_CC2E | GTIM_CCER_CC2P | GTIM_CCER_CC2NP) /* CC2, both edges */
 #  define CCER_PPM_FLIP	GTIM_CCER_CC2P
 # elif HRT_PPM_CHANNEL == 3
-#  define rCCR_PPM	rCCR3			/* capture register for PPM */
-#  define DIER_PPM	GTIM_DIER_CC3IE		/* capture interrupt (non-DMA mode) */
+#  define rCCR_PPM		rCCR3				/* capture register for PPM */
+#  define DIER_PPM		GTIM_DIER_CC3IE		/* capture interrupt (non-DMA mode) */
 #  define SR_INT_PPM	GTIM_SR_CC3IF		/* capture interrupt (non-DMA mode) */
 #  define SR_OVF_PPM	GTIM_SR_CC3OF		/* capture overflow (non-DMA mode) */
-#  define CCMR1_PPM	0			/* not on TI1/TI2 */
-#  define CCMR2_PPM	1			/* on TI3, not on TI4 */
-#  define CCER_PPM	(GTIM_CCER_CC3E | GTIM_CCER_CC3P | GTIM_CCER_CC3NP) /* CC3, both edges */
+#  define CCMR1_PPM		0					/* not on TI1/TI2 */
+#  define CCMR2_PPM		(1 | 0x03 << 4)		/* on TI3, not on TI4 */
+#  define CCER_PPM		(GTIM_CCER_CC3E | GTIM_CCER_CC3P | GTIM_CCER_CC3NP) /* CC3, both edges */
 #  define CCER_PPM_FLIP	GTIM_CCER_CC3P
 # elif HRT_PPM_CHANNEL == 4
-#  define rCCR_PPM	rCCR4			/* capture register for PPM */
-#  define DIER_PPM	GTIM_DIER_CC4IE		/* capture interrupt (non-DMA mode) */
+#  define rCCR_PPM		rCCR4				/* capture register for PPM */
+#  define DIER_PPM		GTIM_DIER_CC4IE		/* capture interrupt (non-DMA mode) */
 #  define SR_INT_PPM	GTIM_SR_CC4IF		/* capture interrupt (non-DMA mode) */
 #  define SR_OVF_PPM	GTIM_SR_CC4OF		/* capture overflow (non-DMA mode) */
-#  define CCMR1_PPM	0			/* not on TI1/TI2 */
-#  define CCMR2_PPM	2			/* on TI3, not on TI4 */
-#  define CCER_PPM	(GTIM_CCER_CC4E | GTIM_CCER_CC4P | GTIM_CCER_CC4NP) /* CC4, both edges */
+#  define CCMR1_PPM		0					/* not on TI1/TI2 */
+#  define CCMR2_PPM		(2 | 0x03 << 4)		/* on TI3, not on TI4 */
+#  define CCER_PPM		(GTIM_CCER_CC4E | GTIM_CCER_CC4P | GTIM_CCER_CC4NP) /* CC4, both edges */
 #  define CCER_PPM_FLIP	GTIM_CCER_CC4P
 # else
 #  error HRT_PPM_CHANNEL must be a value between 1 and 4
