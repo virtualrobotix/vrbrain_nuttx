6bf00d5a458508c3c00572d8507b28d7ca7eec25
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2014-09-19 09:59:34 +0200

    drivers: adding missing variable declaration.

--- src/drivers/drv_pwm_output.h
+++ src/drivers/drv_pwm_output.h
@@ -202,6 +202,9 @@ ORB_DECLARE(output_pwm);
 /** force safety switch off (to disable use of safety switch) */
 #define PWM_SERVO_SET_FORCE_SAFETY_OFF _IOC(_PWM_SERVO_BASE, 23)
 
+/** force safety switch on (to enable use of safety switch) */
+#define PWM_SERVO_SET_FORCE_SAFETY_ON  _IOC(_PWM_SERVO_BASE, 24)
+
 /*
  *
  *
