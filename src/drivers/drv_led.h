f3e95af5bb869c2ba63af97da462172d16221925
LukeMike <luca.micheletti.laser@gmail.com>
2014-12-30 11:25:26 +0100

    DRV_LED: added definition of leds for VR Brain 5.2

--- src/drivers/drv_led.h
+++ src/drivers/drv_led.h
@@ -88,6 +88,15 @@
 #define LED_EXT1		3
 #define LED_EXT2		4
 #define LED_EXT3		5
+#elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V52)
+#define LED_YELLOW		1
+#define LED_BLUE		1
+#define LED_AMBER		2
+#define LED_RED			2
+#define LED_GREEN		0
+#define LED_EXT1		3
+#define LED_EXT2		4
+#define LED_EXT3		5
 #elif defined(CONFIG_ARCH_BOARD_VRUBRAIN_V51)
 #define LED_YELLOW		1
 #define LED_BLUE		1
