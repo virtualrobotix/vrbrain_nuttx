59262dc11ef47573d3276f7bf1a3c36786bb87bc
LukeMike <luca.micheletti.laser@gmail.com>
2014-12-30 13:14:10 +0100

    PWM INPUT: added precompiled archives, makefiles, NuttX and board configurations for all VR boards with PWM Multichannels Radio

---
+++ makefiles/board_vrbrain-v45P.mk
@@ -0,0 +1,11 @@
+#
+# Board-specific definitions for the VRBRAIN 4.5
+#
+
+#
+# Configure the toolchain
+#
+CONFIG_ARCH			 = CORTEXM4F
+CONFIG_BOARD		 = VRBRAIN_V45
+
+include $(VRX_MK_DIR)/toolchain_gnu-arm-eabi.mk
---
+++ makefiles/board_vrbrain-v52P.mk
@@ -0,0 +1,11 @@
+#
+# Board-specific definitions for the VRBRAIN 5.2
+#
+
+#
+# Configure the toolchain
+#
+CONFIG_ARCH			 = CORTEXM4F
+CONFIG_BOARD		 = VRBRAIN_V52
+
+include $(VRX_MK_DIR)/toolchain_gnu-arm-eabi.mk
