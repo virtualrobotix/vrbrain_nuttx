ca02854c18515dacef798601ec7982d6b66374b1
LukeMike <luca.micheletti.laser@gmail.com>
2015-02-03 09:46:32 +0100

    makefiles: added building rgbled module for all VR boards

--- makefiles/config_vrbrain-v51Pro_default.mk
+++ makefiles/config_vrbrain-v51Pro_default.mk
@@ -23,6 +23,7 @@ MODULES		+= drivers/boards/vrbrain-v51Pro
 MODULES		+= drivers/vrbrain/vroutput
 MODULES		+= drivers/vrbrain/vrinput/controls
 MODULES		+= drivers/vrbrain/vrinput
+MODULES		+= drivers/rgbled
 
 
 
