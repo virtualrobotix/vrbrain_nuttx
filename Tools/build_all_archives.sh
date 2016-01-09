d117c778d6f0ff1194d90b8f2c20d34324512aa1
LukeMike <luca.micheletti.laser@gmail.com>
2016-01-09 23:26:44 +0100

    Tools: added scripts to build archives and default firmware of VR boards

---
+++ Tools/build_all_archives.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v45
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v45P
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v51
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v51P
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v51Pro
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v51ProP
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v52
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v52P
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v52Pro
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrbrain-v52ProP
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrubrain-v51
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrubrain-v51P
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrubrain-v52
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrcore-v10
+make -f Makefile.make archives MAXOPTIMIZATION=-Os BOARDS=vrcore-v10P
+
+make distclean
---
+++ Tools/build_all_default.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+make -f Makefile.make clean
+
+make -f Makefile.make vrbrain-v45_default
+make -f Makefile.make vrbrain-v45P_default
+make -f Makefile.make vrbrain-v51_default
+make -f Makefile.make vrbrain-v51P_default
+make -f Makefile.make vrbrain-v51Pro_default
+make -f Makefile.make vrbrain-v51ProP_default
+make -f Makefile.make vrbrain-v52_default
+make -f Makefile.make vrbrain-v52P_default
+make -f Makefile.make vrbrain-v52Pro_default
+make -f Makefile.make vrbrain-v52ProP_default
+make -f Makefile.make vrubrain-v51_default
+make -f Makefile.make vrubrain-v51P_default
+make -f Makefile.make vrubrain-v52_default
+make -f Makefile.make vrcore-v10_default
+make -f Makefile.make vrcore-v10P_default
